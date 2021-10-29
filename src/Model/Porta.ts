efb843de5bdfe5697fbc4d887620a69cecec0d18
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-29 18:01:42 -0300

    Create Model

---
+++ src/Model/Porta.ts
@@ -0,0 +1,30 @@
+export default class Porta {
+    #numero: number
+    #temPresente: boolean
+    #selecionada: boolean
+    #aberta: boolean
+
+    constructor(numero: number, temPresente = false, selecionada = false, aberta = false) {
+        this.#numero = numero
+        this.#temPresente = temPresente
+        this.#selecionada = selecionada
+        this.#aberta = aberta
+    }
+
+    get numero() {
+        return this.#numero
+    }
+
+    get temPresente() {
+        return this.#temPresente
+    }
+
+    get selecionada() {
+        return this.#selecionada
+    }
+
+    get aberta() {
+        return this.#aberta
+    }
+
+}
