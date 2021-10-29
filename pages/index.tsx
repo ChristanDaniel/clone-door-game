d64c3334fdb39eca93b296abb0f3bb6a7e88a72c
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-28 21:01:28 -0300

    criando component Porta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Presente from '../src/components/Presente'
+import Porta from '../src/components/Porta'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Presente />
+      <Porta />
     </div>
   )
 }
---
+++ src/components/Porta/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Porta.module.css'
+
+
+export default function Porta() {
+    return (
+        <div className={styles.Porta}>
+            
+        </div>
+    )
+}
