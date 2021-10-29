cb6dffa4ebca5e482e9a27efd62bb6b07f9c35c8
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-29 17:43:15 -0300

    personalizando component porta

--- src/components/Porta/index.tsx
+++ src/components/Porta/index.tsx
@@ -1,10 +1,21 @@
-import styles from './Porta.module.css'
+import styles from "./Porta.module.css";
 
+type PortaProps = {
+  selecionada: string
+}
 
-export default function Porta() {
-    return (
+export default function Porta(props: PortaProps) {
+    const PortaSelecionada = props.selecionada ? styles.Selecionada : '';
+
+  return (
+    <div className={styles.Area}>
+      <div className={`${styles.Estrutura} ${PortaSelecionada}`}>
         <div className={styles.Porta}>
-            
+          <div className={styles.Numero}>3</div>
+          <div className={styles.Macaneta}></div>
         </div>
-    )
+      </div>
+      <div className={styles.Chao}></div>
+    </div>
+  );
 }
