610e588431f1dd8f234192e98d7e3753ee714892
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-30 15:29:41 -0300

    botão de reiniciar game criado

--- pages/jogo.tsx
+++ pages/jogo.tsx
@@ -1,26 +1,38 @@
-import type { NextPage } from 'next'
-import Presente from '../src/components/Presente'
-import Porta from '../src/components/Porta'
-import PortaModel from '../src/Model/Porta'
-import { useState } from 'react'
-import { atualizarPortas, criarPortas } from '../src/functions/Porta'
+import type { NextPage } from "next";
+import Presente from "../src/components/Presente";
+import Porta from "../src/components/Porta";
+import PortaModel from "../src/Model/Porta";
+import { useState } from "react";
+import { atualizarPortas, criarPortas } from "../src/functions/Porta";
+import Link from "next/link";
 
-import styles  from '../styles/Jogo.module.css'
+import styles from "../styles/Jogo.module.css";
 
 const Jogo: NextPage = () => {
-  const [portas, setPortas] = useState(criarPortas(3,2))
+  const [portas, setPortas] = useState(criarPortas(16, 2));
 
   return (
     <div className={styles.jogo}>
-      {portas.map(porta => {
-        return <Porta 
-        key={porta.numero}
-        value={porta} 
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}
-        />
-      })}
+      <div className={styles.portas}>
+        {portas.map((porta) => {
+          return (
+            <Porta
+              key={porta.numero}
+              value={porta}
+              onChange={(novaPorta) =>
+                setPortas(atualizarPortas(portas, novaPorta))
+              }
+            />
+          );
+        })}
+      </div>
+      <div className={styles.botoes}>
+        <Link href="/" passHref>
+          <button>Reiniciar Jogo</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Jogo
+export default Jogo;
