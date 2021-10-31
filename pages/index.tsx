3e849c45311a1c4e8b4ce5642a3bea2a299000e8
ChristianDaniel <christian_daniel04@hotmail.com>
2021-10-31 15:14:40 -0300

    component cartão criado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
+import Cartao from '../src/components/Cartao'
 
 const Home: NextPage = () => {
 
   return (
     <div>
       <h1>Formulário</h1>
+      <Cartao />
     </div>
   )
 }
---
+++ src/components/Cartao/index.tsx
@@ -0,0 +1,9 @@
+import styles from './cartao.module.css'
+
+export default function Cartao(props) {
+    return (
+        <div className={styles.cartao}>
+
+        </div>
+    )
+}
