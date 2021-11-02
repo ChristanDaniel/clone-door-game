e84c3588c1be4f728ebd4a6abb3cd44ed6f9978a
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-02 13:54:17 -0300

    button de incremento e decremento

--- src/components/EntradaNumerica/index.tsx
+++ src/components/EntradaNumerica/index.tsx
@@ -7,11 +7,23 @@ interface EntradaNumericaProps {
 }
 
 export default function EntradaNumerica(props: EntradaNumericaProps) {
-    
+
+    const inc = () => props.onChange(props.value + 1)
+    const dec = () => props.onChange(props.value - 1) 
+
     return (
         <div className={styles.entradaNumerica}>
             <span className={styles.text}>{props.text}</span>
             <span className={styles.value}>{props.value}</span>
+            <div className={styles.botoes}>
+                {props.value === 0 ? 
+                  <button>-</button> : <button onClick={dec}>-</button>
+                }
+
+                {props.value === 20 ? 
+                  <button>+</button> : <button onClick={inc}>+</button>
+                }
+            </div>
         </div>
     )
 }
